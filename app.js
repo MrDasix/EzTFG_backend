015bc6e0351a6d209884da77606dbb2bd4b1d57f
David Sirera <dasix.98@gmail.com>
2018-11-18 00:15:49 +0100

    funciona a la perfeccio

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var bodyParser = require('body-parser');
 var MongoClient = mongo.MongoClient;
 
 const app = express();
-const port = 3999;
+const port = 3000;
 
 var mongo = require('./db.js');
 
@@ -15,15 +15,30 @@ app.use(bodyParser.urlencoded({
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => res.render("index"));
-app.get("/login",(req,res) => res.render("login"));
 app.get("/register",(req,res) => res.render("register"));
-app.get("/main",(req,res)=>res.render("main"));
+
+app.get("/login",function(req,res){
+    var username = req.query.username;
+    var password = req.query.password;
+
+    mongo.login(username,password,function(err,result){
+        if(err){
+            res.render("Error");
+        }else if(result === null){
+            res.render("Error, aquest usuari i contrasenya no existeix");
+        }else{
+            res.status(200).send(result);
+            console.log(result);
+        }
+    });
+});
 
 app.post("/register",function(req,res){
     var username = req.body.username;
     var password = req.body.password;
+    var is_professor = req.body.is_professor;
 
-    mongo.register(username,password,function(err,result){
+    mongo.register(username,password,is_professor,function(err,result){
         if(err){
             res.render("Error");
         }else{
@@ -32,18 +47,38 @@ app.post("/register",function(req,res){
     });
 });
 
-app.post("/login",function(req,res){
+
+app.post("/insert_tfg",function(req,res){
     var username = req.body.username;
     var password = req.body.password;
+    var is_professor = req.body.is_professor;
 
-    mongo.login(username,password,function(err,result){
+    mongo.insert_tfg(title,is_professor,email,function(err,result){
+        if(err){
+            res.render("Error la db no furula");
+        }else{
+            res.redirect("/");
+        }
+    });
+});
+
+app.get("/get_tfg",function(req,res){
+    mongo.get_tfg(function(err,result){
         if(err){
             res.render("Error");
         }else if(result === null){
-            res.render("Error, aquest usuari i contrasenya no existeix");
+            res.render("Error, no hi ha cap tfg");
         }else{
-            res.redirect("/main");
+            res.status(200).send(result);
         }
+    });
+});
+
+app.get("/reset",function(req,res){
+    mongo.resetdb(function(err,result){
+
+        console.log("Reset Data");
+
     });
 });
 
--- db.js
+++ db.js
@@ -22,7 +22,7 @@ module.exports = {
             }
         });
     },
-    register: function(username, password, callback){
+    register: function(username, password, is_professor, callback){
         MongoClient.connect(url, function(err, client) {
             if (err) {
                 console.log('Unable to connect to the mongoDB server. Error: ', err);
@@ -31,7 +31,8 @@ module.exports = {
                 var db = client.db("tfg");
                 var newUser = {
                     username:username,
-                    password:password
+                    password:password,
+                    is_professor: is_professor
                 };
                 db.collection("users").insertOne(newUser, function(err, res) {
                     callback(err, res);
@@ -40,5 +41,61 @@ module.exports = {
                 });
             }
         });
-    }
+    },
+    
+    resetdb: function(callback){
+        MongoClient.connect(url, function(err, client) {
+            if (err) {
+                console.log('Unable to connect to the mongoDB server. Error: ', err);
+                callback(err, null);
+            } else {
+                var db = client.db("tfg");
+                db.dropDatabase();
+            }
+        });
+    },
+    get_tfg: function(callback){
+        MongoClient.connect(url, function(err, client) {
+            if (err) {
+                console.log('Unable to connect to the mongoDB server. Error: ', err);
+                callback(err, null);
+            } else {
+                var db = client.db("tfg");
+                db.collection("tfgs").find(function(err, result) {
+                    callback(err, result);
+                    client.close();
+                });
+            }
+        });
+    },
+    insert_tfg: function(title,is_professor,email,callback){
+        MongoClient.connect(url, function(err, client) {
+            if (err) {
+                console.log('Unable to connect to the mongoDB server. Error: ', err);
+                callback(err, null);
+            } else {
+                var db = client.db("tfg");
+                var newTFG;
+                if(is_professor){
+                    newTFG = {
+                        title:title,
+                        professor:email,
+                        alumne:null
+                    }
+                }
+               else {
+                    newTFG = {
+                        title:title,
+                        professor:null,
+                        alumne:email
+                    };
+                }
+                db.collection("tfgs").insertOne(newTFG, function(err, res) {
+                    callback(err, res);
+                    console.log("tfg "+title+ " by " + email  +" added correctly");
+                    client.close();
+                });
+            }
+        });
+    },
 }
