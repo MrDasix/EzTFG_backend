a38c1574ad90f61d92c7ed464ccf242c07c0d3cd
David Sirera <dasix.98@gmail.com>
2018-11-17 19:40:55 +0100

    Funciona tot menys el mongodb

--- app.js
+++ app.js
@@ -1,28 +1,33 @@
 var mongo = require("mongodb");
 var express = require("express");
+var bodyParser = require('body-parser');
 var MongoClient = mongo.MongoClient;
 
 const app = express();
-const port = 6000;
+const port = 3340;
 
 var mongo = require('./db.js');
 
+app.use(bodyParser.json() );
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => res.render("index"));
-app.get("/login",(req,res) => res.send("Log In"));
-app.get("/register",(req,res) => res.send("Register"));
-app.get("/main",(req,res)=>res.send("Enhorabona has entrat al nosttre domini web 2003 copyright"))
+app.get("/login",(req,res) => res.render("login"));
+app.get("/register",(req,res) => res.render("register"));
+app.get("/main",(req,res)=>res.render("main"));
 
 app.post("/register",function(req,res){
     var username = req.body.username;
     var password = req.body.password;
 
-    mongo.register(username,passord,function(err,result){
+    mongo.register(username,password,function(err,result){
         if(err){
             res.render("Error");
         }else{
-            app.redirect("/");
+            res.redirect("/");
         }
     });
 });
@@ -31,18 +36,18 @@ app.post("/login",function(req,res){
     var username = req.body.username;
     var password = req.body.password;
 
-    mongo.login(username,passord,function(err,result){
+    mongo.login(username,password,function(err,result){
         if(err){
             res.render("Error");
         }else if(result === null){
             res.render("Error, aquest usuari i contrasenya no existeix");
         }else{
-            app.redirect("/main");
+            res.redirect("/main");
         }
     });
 });
 
-app.get("*",(req,res) =>app.redirect("/"));
+app.get("*",(req,res) => res.redirect("/"));
 app.listen(port, () => console.log(`Database listening on port ${port}!`));
 
 
