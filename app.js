e9611470c5e99f6d40140b840c7962f13a93ba8c
David Sirera <dasix.98@gmail.com>
2018-11-18 10:08:32 +0100

    SuperFuncional

--- app.js
+++ app.js
@@ -15,7 +15,6 @@ app.use(bodyParser.urlencoded({
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => res.render("index"));
-app.get("/register",(req,res) => res.render("register"));
 
 app.get("/login",function(req,res){
     var username = req.query.username;
@@ -34,6 +33,7 @@ app.get("/login",function(req,res){
 });
 
 app.post("/register",function(req,res){
+    console.log("register executed");
     var username = req.body.username;
     var password = req.body.password;
     var is_professor = req.body.is_professor;
@@ -49,10 +49,10 @@ app.post("/register",function(req,res){
 
 
 app.post("/insert_tfg",function(req,res){
-    var username = req.body.username;
-    var password = req.body.password;
+    console.log("Insert_tfg executed");
+    var title = req.body.title;
+    var email = req.body.email;
     var is_professor = req.body.is_professor;
-
     mongo.insert_tfg(title,is_professor,email,function(err,result){
         if(err){
             res.render("Error la db no furula");
@@ -62,11 +62,51 @@ app.post("/insert_tfg",function(req,res){
     });
 });
 
+app.post("/remove_tfg",function(req,res){
+    console.log("remove_tfg executed");
+    var title = req.body.title;
+    mongo.remove_tfg(title,function(err,result){
+        if(err){
+            res.render("Error la db no furula");
+        }else{
+            res.redirect("/");
+        }
+    });
+});
+
+app.post("/add_tfg_description",function(req,res){
+    console.log("remove_tfg executed");
+    var title = req.body.title;
+    var description = req.body.description;
+    mongo.add_tfg_description(title,description,function(err,result){
+        if(err){
+            res.render("Error la db no furula");
+        }else{
+            res.redirect("/");
+        }
+    });
+});
+
+app.post("/add_user_to_tfg",function(req,res){
+    console.log("add_user_to_tfg executed");
+    var title = req.body.title;
+    var user = req.body.user;
+    var is_professor = req.body.is_professor;
+    mongo.add_user_to_tfg(title,user,is_professor,function(err,result){
+        if(err){
+            res.render("Error la db no furula");
+        }else{
+            res.redirect("/");
+        }
+    });
+});
+
+
 app.get("/get_tfg",function(req,res){
     mongo.get_tfg(function(err,result){
         if(err){
             res.render("Error");
-        }else if(result === null){
+        }else if(res === null){
             res.render("Error, no hi ha cap tfg");
         }else{
             res.status(200).send(result);
@@ -74,12 +114,58 @@ app.get("/get_tfg",function(req,res){
     });
 });
 
-app.get("/reset",function(req,res){
-    mongo.resetdb(function(err,result){
-
-        console.log("Reset Data");
-
+app.get("/get_tfg_user",function(req,res){
+    console.log("get_tfg_user is executed");
+    var user = req.query.user;
+    mongo.get_tfg_user(user,function(err,result){
+        if(err){
+            res.render("Error");
+        }else if(res === null){
+            res.render("Error, no hi ha cap tfg d'aquest user");
+        }else{
+            res.status(200).send(result);
+        }
     });
+});
+
+app.get("/get_tfg_by_title",function(req,res){
+    console.log("get_tfg_by_title is executed");
+    var title = req.query.title;
+    mongo.get_tfg_by_title(title,function(err,result){
+        if(err){
+            res.render("Error");
+        }else if(res === null){
+            res.render("Error, no hi ha cap tfg d'aquesta title");
+        }else{
+            res.status(200).send(result);
+        }
+    });
+});
+
+app.get("/get_tfg_disponibles",function(req,res){
+    console.log("get_tfg_disponibles is executed");
+    var is_professor = req.query.is_professor;
+    mongo.get_tfg_disponibles(is_professor,function(err,result){
+        if(err){
+            res.render("Error");
+        }else if(res === null){
+            res.render("Error, no hi ha cap tfg fet per profes");
+        }else{
+            res.status(200).send(result);
+        }
+    });
+});
+
+
+
+app.get("/reset",function(req,res){
+   // mongo.insert_tfg("tfg_de_profe",true,"2",function(err,res){
+
+  // });
+
+   // mongo.remove_tfg("soc un nou tfg",function(err,res){
+
+    //});
 });
 
 app.get("*",(req,res) => res.redirect("/"));
--- db.js
+++ db.js
@@ -2,6 +2,8 @@ var MongoClient = require("mongodb").MongoClient;
 
 var url = "mongodb://localhost:27017/tfg";
 
+var id_tfg = 1;
+
 module.exports = {
 
     login: function(username,password,callback){
@@ -16,6 +18,8 @@ module.exports = {
                     password:password
                 };
                 db.collection("users").findOne(oldUser, function(err, result) {
+                   // console.log("login successful for :" + username);
+                    console.log(result);
                     callback(err, result);
                     client.close();
                 });
@@ -32,7 +36,7 @@ module.exports = {
                 db.collection("users").findOne( { username: username },function(err,result){
 
                     if(result != null){
-                        console.log('amic que fas¿¿¿ja existeix loco');
+                        console.log('amic que fas¿¿¿ja existeix loco (el registerr)');
                         callback(err,result);
                         client.close();
                     }
@@ -70,42 +74,218 @@ module.exports = {
                 console.log('Unable to connect to the mongoDB server. Error: ', err);
                 callback(err, null);
             } else {
-                var db = client.db("tfg");
-                db.collection("tfgs").find(function(err, result) {
+                var db = client.db("tfg"); 
+                db.collection("tfgs").find({}).toArray(function(err, result) {
                     callback(err, result);
                     client.close();
                 });
             }
         });
     },
-    insert_tfg: function(title,is_professor,email,callback){
+
+    get_tfg_user: function(user,callback){
+        MongoClient.connect(url, function(err, client) {
+            if (err) {
+                console.log('Unable to connect to the mongoDB server. Error: ', err);
+                callback(err, null);
+            } else {
+                var db = client.db("tfg"); 
+                db.collection("tfgs").find({ $or : [{alumne:user},{professor:user}]}).toArray(function(err, result) {
+                   // console.log(result);
+                    callback(err, result);
+                    client.close();
+                });
+            }
+        });
+    },
+
+    get_tfg_disponibles: function(is_professor,callback){
+        MongoClient.connect(url, function(err, client) {
+            if (err) {
+                console.log('Unable to connect to the mongoDB server. Error: ', err);
+                callback(err, null);
+            } else {
+                var db = client.db("tfg"); 
+                if(is_professor=="true"){
+                    db.collection("tfgs").find({professor:null}).toArray(function(err, result) {
+                            callback(err, result);
+                            client.close();
+                        });
+                }else{
+                    db.collection("tfgs").find({alumne:null}).toArray(function(err, result) {
+                     console.log("else");
+                     console.log(result);
+                        callback(err, result);
+                        client.close();
+                    });
+                }
+            }
+        });
+    },
+
+    get_tfg_by_title: function(title,callback){
         MongoClient.connect(url, function(err, client) {
             if (err) {
                 console.log('Unable to connect to the mongoDB server. Error: ', err);
                 callback(err, null);
             } else {
                 var db = client.db("tfg");
-                var newTFG;
-                if(is_professor){
-                    newTFG = {
-                        title:title,
-                        professor:email,
-                        alumne:null
-                    }
-                }
-               else {
-                    newTFG = {
-                        title:title,
-                        professor:null,
-                        alumne:email
-                    };
-                }
-                db.collection("tfgs").insertOne(newTFG, function(err, res) {
-                    callback(err, res);
-                    console.log("tfg "+title+ " by " + email  +" added correctly");
+                console.log('title im looking for is : ' + title);
+                var oldtfg = {
+                    title:title
+                }; 
+                db.collection("tfgs").findOne(oldtfg,function(err, result) {
+                    console.log(result);
+                    callback(err, result);
                     client.close();
                 });
             }
         });
     },
+
+
+    insert_tfg: function(title,is_professor,email,callback){
+        //console.log(' hola amigo : ' + title + ' ' + is_professor + ' ' + email);
+        MongoClient.connect(url, function(err, client) {
+            if(title===''){
+                console.log('title null ');
+                callback(err, null);             
+            }
+            else if (err) {
+                console.log('Unable to connect to the mongoDB server. Error: ', err);
+                callback(err, null);
+            } else {
+                var db = client.db("tfg");
+
+                db.collection("tfgs").findOne( { title: title },function(err,result){
+
+                    if(result != null){
+                        console.log('amic que fas¿¿¿ja existeix loco (el insert de tfg de '+ title + ' )');
+                        callback(err,result);
+                        client.close();
+                    }
+                    else{
+                        var newTFG;
+                        if(is_professor){
+                            newTFG = {
+                                title:title,
+                                professor:email,
+                                alumne:null,
+                                creador:email,
+                                description:null,
+                                id:id_tfg
+                            }
+                        }
+                        else {
+                            newTFG = {
+                                title:title,
+                                professor:null,
+                                alumne:email,
+                                creador:email,
+                                description:null,
+                                id:id_tfg
+                            };
+                        }
+                        console.log("li he posat la id " + id_tfg);
+                        ++id_tfg;
+                        db.collection("tfgs").insertOne(newTFG, function(err, res) {
+                            callback(err, res);
+                            console.log("tfg "+title+ " by " + email + " added correctly");
+                            client.close();
+                        });
+                    }
+                });    
+            }
+        });
+    },
+    remove_tfg: function(title,callback){
+        MongoClient.connect(url, function(err, client) {
+            if (err) {
+                console.log('Unable to connect to the mongoDB server. Error: ', err);
+                callback(err, null);
+            } else {
+                var db = client.db("tfg");
+
+                db.collection("tfgs").findOne( { title: title },function(err,result){
+
+                    if(result == null){
+                        console.log('amic que fas¿¿¿NO existeix loco');
+                        callback(err,result);
+                        client.close();
+                    }
+                    else{
+                        db.collection("tfgs").deleteOne({title:title}, function(err, res) {
+                            callback(err, res);
+                            console.log("tfg "+title  +" removed correctly");
+                            client.close();
+                        });
+                    }
+                });    
+            }
+        });
+    },
+
+    add_tfg_description: function(title,description,callback){
+        MongoClient.connect(url, function(err, client) {
+            if (err) {
+                console.log('Unable to connect to the mongoDB server. Error: ', err);
+                callback(err, null);
+            } else {
+                var db = client.db("tfg");
+
+                db.collection("tfgs").findOne( { title: title },function(err,result){
+
+                    if(result == null){
+                        console.log('amic que fas¿¿¿NO existeix loco');
+                        callback(err,result);
+                        client.close();
+                    }
+                    else{
+                        db.collection("tfgs").updateOne({title:title},{$set:{"description":description} }, function(err, res) {
+                            callback(err, res);
+                            console.log("tfg "+title  +" description updated correctly");
+                            client.close();
+                        });
+                    }
+                });    
+            }
+        });
+    },
+
+    add_user_to_tfg: function(title,user,is_professor,callback){
+        MongoClient.connect(url, function(err, client) {
+            if (err) {
+                console.log('Unable to connect to the mongoDB server. Error: ', err);
+                callback(err, null);
+            } else {
+                var db = client.db("tfg");
+
+                db.collection("tfgs").findOne( { title: title },function(err,result){
+
+                    if(result == null){
+                        console.log('amic que fas¿¿¿NO existeix loco');
+                        callback(err,result);
+                        client.close();
+                    }
+                    else{
+                        if(is_professor){
+                            db.collection("tfgs").updateOne({title:title},{$set:{"professor":user} }, function(err, res) {
+                                callback(err, res);
+                                console.log("tfg "+title  +" alumne updated correctly");
+                                client.close();
+                            });
+                        }else{
+                            db.collection("tfgs").updateOne({title:title},{$set:{"alumne":user} }, function(err, res) {
+                                callback(err, res);
+                                console.log("tfg "+title  +" professor updated correctly");
+                                client.close();
+                            });
+                        }
+                    }
+                });    
+            }
+        });
+    },
+
+
 }
